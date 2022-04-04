f71ad977bf5342d39f686a70a72b2ffa2b443a32
shahin597 <shahinimrancse@gmail.com>
2022-04-05 02:49:20 +0600

    update-home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import img from '../assets/hero-1.jpg';
+import img from '../assets/hero-1.jpeg';
 import HomeReview from '../HomeReview/HomeReview';
 const Home = () => {
     return (
@@ -9,8 +9,8 @@ const Home = () => {
           <div className='container py-5'>
             <div className="row">
                 <div className="col-12 col-md-6 pt-5">
-                    <h5 className='text-success'>Learn Web Development</h5>
-                    <h2 className='fw-700 fs-1'>Online Course Tutorial From Qualified Instructor</h2>
+                    <h2 className='text-success'>Learn Web Development</h2>
+                    <h4 className='fw-700 fs-1'>Online Course Tutorial From Qualified Instructor</h4>
                     <button className='btn btn-success mt-5'>Explore More</button>
                 </div>
                 <div className="col-12 col-md-6">
