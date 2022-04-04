d8806062d4663848cb98c4ee9469626c630cc58d
shahin597 <shahinimrancse@gmail.com>
2022-04-05 02:27:09 +0600

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,9 +9,9 @@ const Home = () => {
           <div className='container py-5'>
             <div className="row">
                 <div className="col-12 col-md-6 pt-5">
-                    <h5 className='text-success'>More than 4000+ student records available</h5>
-                    <h2 className='fw-700 fs-1'>OnlineLearning - Online Course Tutorial From Top Instructor</h2>
-                    <button className='btn btn-success mt-5'>View All Course</button>
+                    <h5 className='text-success'>Learn Web Development</h5>
+                    <h2 className='fw-700 fs-1'>Online Course Tutorial From Qualified Instructor</h2>
+                    <button className='btn btn-success mt-5'>Explore More</button>
                 </div>
                 <div className="col-12 col-md-6">
                     <img className='img-fluid' src={img} alt="" />
