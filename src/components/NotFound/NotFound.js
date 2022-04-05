fe14a127612d2742f3fd9b0baa40d79aaa3f3af7
shahin597 <shahinimrancse@gmail.com>
2022-04-05 12:41:20 +0600

    complete-task

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
         <img className='mt-5' src={error} alt="error" />
         <div className='mt-2 container d-flex justify-content-center'>
           
-            <h3 className='bg-secondary mt-3 p-3 text-warning w-50 '>Not Found </h3>
+            <h3 className='bg-secondary mt-3 p-3 text-warning w-50 '>Try Existing Page in Navbar</h3>
         </div>
         </>
     );
