0a6cb80c8253584ef6acb8a51c7bda52152509d5
shahin597 <shahinimrancse@gmail.com>
2022-04-05 12:11:59 +0600

    not-found

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import error from '../assets/error.webp';
+import error from '../assets/404.png';
 const NotFound = () => {
     return (
         <>
         <img className='mt-5' src={error} alt="error" />
         <div className='mt-2 container d-flex justify-content-center'>
           
-            <h3 className='bg-secondary mt-3 p-3 text-warning w-50 '>No Url Found. ):</h3>
+            <h3 className='bg-secondary mt-3 p-3 text-warning w-50 '>Not Found </h3>
         </div>
         </>
     );
